69cd44ccc761fbe5e9e8caf46d46b0a87f783dbd
Saksham Sharma <sakshamshankyan7@gmail.com>
2024-10-21 23:16:35 +0530

    just removed 1 line

--- src/component/movie/api/ApiClient.js
+++ src/component/movie/api/ApiClient.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const apiClient = axios.create({
   baseURL: "http://localhost:5000",
   headers: {
-    // 'Content-Type': 'application/json',
+    'Content-Type': 'application/json',
   },
 });
